c0d54757648ff6ae7dc6a36c3f84d2ad3f709083
Lucas Jose da Silva <07055338166@tocantins.governo>
2024-08-13 18:33:36 -0300

    ...

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,8 +1,40 @@
+"use client"
+
+import { useState } from "react";
+import LoginRequest from "../interface/LoginUser/page";
 
 
 
-export default function Auth(){
+export default function Auth() {
 
-return(<h1>Página de Login</h1>);
+    const[email, setEmail] = useState("");
+    const[password , setPassword] = useState("");
+
+    const loginRequest : LoginRequest ={
+        email:email,
+        password:password
+    }
+
+    return (
+
+        <div className="">
+
+            <h1>Página de Login </h1>
+
+            <div>
+                <div>
+                    <label>Email</label>
+                    <input type="text" id="email"></input>
+                </div>
+                <div>
+                    <label>Senha</label>
+                    <input type="password" id="password"></input>
+                </div>
+            </div>
+
+
+
+        </div>
+    );
 
 }
